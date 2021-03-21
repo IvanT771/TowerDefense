c9df4bf7f546a82393932d54b86cad677abc2683
IvanT771 <cnyazev.ivan@yandex.ru>
2021-03-21 16:41:32 +0300

    add tow
    add tow

--- Assets/Scripts/TowerDef.cs
+++ Assets/Scripts/TowerDef.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class TowerDef : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform ball;
+    public Transform shootPoint;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
