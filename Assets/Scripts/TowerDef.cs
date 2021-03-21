f83845ecbf13bc8e50386d3e00fff7a3fca3b110
IvanT771 <cnyazev.ivan@yandex.ru>
2021-03-21 16:53:46 +0300

    Add enemu tag

--- Assets/Scripts/TowerDef.cs
+++ Assets/Scripts/TowerDef.cs
@@ -7,4 +7,19 @@ public class TowerDef : MonoBehaviour
     public Transform ball;
     public Transform shootPoint;
 
+    public Transform target;
+    public float range = 15f;
+
+    void FindEnemu()
+    {
+        var enemus = GameObject.FindGameObjectsWithTag("Enemu");
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+        
+    }
 }
